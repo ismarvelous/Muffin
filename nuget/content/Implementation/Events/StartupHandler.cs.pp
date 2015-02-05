03f6853776741f8c0a7c0dc5cb5092d594bb75b2
Jeroen Wijdeven <jwijdeven@chuhukon.nl>
2015-02-05 15:57:30 +0100

    fixes and new package

--- nuget/content/Implementation/Events/StartupHandler.cs.pp
+++ nuget/content/Implementation/Events/StartupHandler.cs.pp
@@ -1,14 +1,13 @@
-﻿using System.Web.Mvc;
+﻿using System.Reflection;
+using System.Web.Mvc;
 using Autofac;
 using Autofac.Integration.Mvc;
 using Autofac.Integration.WebApi;
 using Muffin.Controllers;
 using Muffin.Core;
-using Muffin.Events;
 using Muffin.Infrastructure;
-using Muffin.Mvc;
+using Umbraco.Core;
 using Umbraco.Web;
-using Umbraco.Web.Mvc;
 using Umbraco.Web.WebApi;
 
 namespace $rootnamespace$.Implementation.Events
@@ -16,14 +15,14 @@ namespace $rootnamespace$.Implementation.Events
 	public class StartupHandler : FoundationEventHandler
 	{
 		public override void InitializeAtStartup(
-			Umbraco.Core.UmbracoApplicationBase umbracoApplication,
-			Umbraco.Core.ApplicationContext applicationContext,
+			UmbracoApplicationBase umbracoApplication,
+			ApplicationContext applicationContext,
 			out IDependencyResolver resolver)
 		{
 			var builder = new ContainerBuilder();
 			builder.RegisterApiControllers(typeof(UmbracoApiController).Assembly);
-			builder.RegisterControllers(typeof(DynamicBaseController).Assembly);
-			builder.RegisterControllers(System.Reflection.Assembly.GetExecutingAssembly());
+			builder.RegisterControllers(typeof(BaseController).Assembly);
+			builder.RegisterControllers(Assembly.GetExecutingAssembly());
 
 			builder.Register(s => new SiteRepository(
 				applicationContext.Services.ContentService,
@@ -32,7 +31,7 @@ namespace $rootnamespace$.Implementation.Events
 					.As<ISiteRepository>()
 					.InstancePerHttpRequest();
 
-			builder.Register(s => new Mapper())
+            builder.Register(s => new Mapper())
                 .As<IMapper>()
                 .InstancePerHttpRequest();
 
