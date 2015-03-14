cb5bb246625b9a8ad461ef85623902a9bea3762c
Jeroen Wijdeven <jwijdeven@chuhukon.nl>
2015-03-14 13:04:35 +0100

    be sure the key and de safefilename are the same.

--- Muffin/Infrastructure/Converters/BaseConverter.cs
+++ Muffin/Infrastructure/Converters/BaseConverter.cs
@@ -7,30 +7,6 @@ using Umbraco.Web;
 
 namespace Muffin.Infrastructure.Converters
 {
-    //[Obsolete("Make use of the new BaseTypeConverter")]
-    //public abstract class BaseConverter : PropertyValueConverterBase
-    //{
-    //    protected ISiteRepository Repository;
-    //    protected IMapper Mapper;
-    //    protected UmbracoHelper Helper;
-
-    //    protected BaseConverter() 
-    //    {
-    //        Initialize(DependencyResolver.Current.GetService<ISiteRepository>(), DependencyResolver.Current.GetService<IMapper>());
-    //    }
-
-    //    protected BaseConverter(ISiteRepository rep, IMapper mapper)
-    //    {
-    //        Initialize(rep, mapper);
-    //    }
-
-    //    protected void Initialize(ISiteRepository rep, IMapper map)
-    //    {
-    //        Repository = rep;
-    //        Mapper = map;
-    //    }
-    //}
-
     public abstract class BaseTypeConverter : TypeConverter
     {
         protected ISiteRepository Repository;
